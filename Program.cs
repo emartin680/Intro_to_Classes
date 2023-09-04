3338d3f60ce4018f03de62b7a7ca5558f4cfbb17
emartin680 <emartin680@c2kni.net>
2023-09-04 15:14:42 +0100

    V.1

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBJECTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human aHuman = new Human();
+            Console.WriteLine("The human's name is " + aHuman.getName());
+            aHuman.setName("Andrew");
+            Console.WriteLine("The human's new name is " + aHuman.getName());
+            Human bHuman = new Human();
+            Console.WriteLine("The human's name is " + bHuman.getName());
+            bHuman.setName("Jason");
+            Console.WriteLine("The human's new name is " + bHuman.getName());
+
+            Console.ReadKey();
+        }
+    }
+
+    class Human
+    {
+        //Instance variables
+        string name = "dave";
+        char gender;
+        int age;
+        //Default constructor
+        public Human() 
+        { 
+
+        }
+
+        //This is a setter-method for name
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+
+
+    }
+   
+}
