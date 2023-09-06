ff72a7cee2d53da0433f2d31379cb45cdf64ecc0
emartin680 <emartin680@c2kni.net>
2023-09-06 13:09:00 +0100

    V.3

--- Program.cs
+++ Program.cs
@@ -9,43 +9,131 @@ namespace OBJECTS
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Human aHuman = new Human();
-            Console.WriteLine("The human's name is " + aHuman.getfName());
-            aHuman.setfName("Andrew");
-            Console.WriteLine("The human's new name is " + aHuman.getfName());
+        { 
+            //This is a constructor call. This creates a new instance (object) of the class human
+            Human aHuman = new Human();//This triggers the default constructor method
+            
+            //This will call getName() instance getter method for the instance variable name
+            Console.WriteLine("The human's name is " + aHuman.getName());
+            
+            //This will call the setName() and set the name tyo "Andrew" using setter method for the instance variable name
+            aHuman.setName("Andrew");
+            
+            //This will call the getName() getter method which should show the name for this object (aHuman) changed to Andrew
+            Console.WriteLine("The human's new name is " + aHuman.getName());
+            
+            //This will create a new human object- bHuman
             Human bHuman = new Human();
-            Console.WriteLine("The human's name is " + bHuman.getfName());
-            bHuman.setfName("Jason");
-            Console.WriteLine("The human's new name is " + bHuman.getfName());
+            Console.WriteLine("The human's name is " + bHuman.getName());
+            bHuman.setName("Jason");
+            Console.WriteLine("The human's new name is " + bHuman.getName());
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your gender?");
+            char gender = Convert.ToChar(Console.ReadLine());
+            int age;
+            //This loop will ensure that the user enters an integer as an age before allowing them to proceed
+            do
+            {
+                Console.WriteLine("What is your age?");
+            }
+            while (!Int32.TryParse(Console.ReadLine(), out age));
+
+            //This will call the parameterised contructor initialising name, gender and age
+            Human cHuman = new Human(name, gender, age);
+            
+            //The following two statements use placeholders to allow the insertion of variables into an output string
+            Console.WriteLine("Your name is {0}. You are a {1}. You are {2} years of age", cHuman.getName(), cHuman.getGender(), cHuman.getAge());
+            Console.WriteLine($"Your name is {cHuman.getName()}. You are a {cHuman.getGender()}. You are {cHuman.getAge()} years of age");
+
+            bool moreWords = true;
+            do
+            {
+                Console.WriteLine("Please enter the word spoken by your child?");
+                aHuman.setSpokenWords(Console.ReadLine());
+                Console.WriteLine("Would you like to add another word- enter \"y\" or \"n\"");
+                if(Console.ReadLine().ToLower()=="n")
+                {
+                    moreWords = false;
+                }
+            }
+            while (moreWords);
+
+            List<string> kidsWords = new List<string>();
+            kidsWords = aHuman.getSpokenWords();
+            foreach (string word in kidsWords)
+            {
+                Console.WriteLine(word);
+            }
             Console.ReadKey();
         }
     }
-
     class Human
     {
-        // Instance variables for objects created from this class
-        string fName = "dave"; // All instance objects will, by default, have the name "Dave"
-        string surname;
+        //Instance variables
+        //These are private by default unless an access modifiers is specified i.e. public 
+        string name = "dave";
         char gender;
         int age;
-        double height;
-        // Default constructor. This constructor has no parameters.
-        public Human() 
-        { 
+        List<string> spokenWords = new List<string>();//This will create a list of string for each object
 
-        }
-
-        // This is a setter-method for full name which will enable users of the program to change the full name of that particular object
-        public void setfName(string fName)
+        //Default constructor
+        public Human()
         {
-            this.fName = fName;
+
         }
 
-        // This is a getter method which will return the value stored against that particular instance variable
-        public string getfName()
+        //This constructor takes three parameters the order, type and number of which is significan
+        public Human(string name, char gender, int age)
         {
-            return fName;
+            // this. refers to the instance variables of the same name as the variable being passed through the constructor call
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
         }
-    } 
+
+        //The following are GETTERs & SETTERs for instance variables name, gender and age.
+        //These are public to ensure 
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setGender(char gender)
+        {
+            this.gender=gender;
+        }
+
+        public char getGender()
+        {
+            return gender;
+        }
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+        public void setSpokenWords(string word)
+        {
+            //This will add words to the list of strings for each object
+            spokenWords.Add(word);
+        }
+
+        //This will return the list of spoken words
+        public List<string> getSpokenWords()
+        {
+            return spokenWords;
+        }
+            
+    }
+   
 }
